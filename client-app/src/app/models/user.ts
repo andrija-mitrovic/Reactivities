6af8a69f2d9eb537a88ac8e2772d01ed10f6940e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-08-28 08:21:11 +0200

    Added registration and login form on the frontend

---
+++ client-app/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    username: string;
+    display: string;
+    token: string;
+    image?: string;
+}
+
+export interface UserFormValues {
+    email: string;
+    password: string;
+    displayName?: string;
+    username?: string;
+}
